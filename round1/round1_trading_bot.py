1a027bdff755493d784bc64f9957235116c7ce11
pauldefressenel <pdefressenel@hotmail.fr>
2025-07-05 14:06:55 +0200

    Our strategies

--- round1/round1_trading_bot.py
+++ round1/round1_trading_bot.py
@@ -1,1 +1,168 @@
+from datamodel import OrderDepth, TradingState, Order
+from typing import List
 import pandas as pd
+import numpy as np
+from collections import deque
+from abc import abstractmethod
+
+class Strategy:
+    def __init__(self, symbol: str, limit: int) -> None:
+        self.symbol = symbol
+        self.limit = limit
+
+    @abstractmethod
+    def act(self, state: TradingState) -> None:
+        raise NotImplementedError()
+
+    def run(self, state: TradingState) -> list[Order]:
+        self.orders = []
+        self.act(state)
+        return self.orders
+
+    def buy(self, price: int, quantity: int) -> None:
+        self.orders.append(Order(self.symbol, price, quantity))
+
+    def sell(self, price: int, quantity: int) -> None:
+        self.orders.append(Order(self.symbol, price, -quantity))
+
+class MarketMakingStrategy(Strategy):
+    def __init__(self, symbol: str, limit: int) -> None:
+        super().__init__(symbol, limit)
+        self.window = deque()
+        self.window_size = 10
+
+    @abstractmethod
+    def get_true_value(self, state: TradingState) -> int:
+        raise NotImplementedError()
+
+    def act(self, state: TradingState) -> None:
+        true_value = self.get_true_value(state)
+
+        order_depth = state.order_depths[self.symbol]
+        buy_orders = sorted(order_depth.buy_orders.items(), reverse=True)
+        sell_orders = sorted(order_depth.sell_orders.items())
+
+        position = state.position.get(self.symbol, 0)
+        to_buy = self.limit - position
+        to_sell = self.limit + position
+
+        self.window.append(abs(position) == self.limit)
+        if len(self.window) > self.window_size:
+            self.window.popleft()
+
+        soft_liquidate = len(self.window) == self.window_size and sum(self.window) >= self.window_size / 2 and self.window[-1]
+        hard_liquidate = len(self.window) == self.window_size and all(self.window)
+
+        max_buy_price = true_value - 1 if position > self.limit * 0.5 else true_value
+        min_sell_price = true_value + 1 if position < self.limit * -0.5 else true_value
+
+        for price, volume in sell_orders:
+            if to_buy > 0 and price <= max_buy_price:
+                quantity = min(to_buy, -volume)
+                self.buy(price, quantity)
+                to_buy -= quantity
+
+        if to_buy > 0 and hard_liquidate:
+            quantity = to_buy // 2
+            self.buy(true_value, quantity)
+            to_buy -= quantity
+
+        if to_buy > 0 and soft_liquidate:
+            quantity = to_buy // 2
+            self.buy(true_value - 2, quantity)
+            to_buy -= quantity
+
+        if to_buy > 0 and buy_orders:
+            popular_buy_price = max(buy_orders, key=lambda tup: tup[1])[0]
+            price = min(max_buy_price, popular_buy_price + 1)
+            self.buy(price, to_buy)
+
+        for price, volume in buy_orders:
+            if to_sell > 0 and price >= min_sell_price:
+                quantity = min(to_sell, volume)
+                self.sell(price, quantity)
+                to_sell -= quantity
+
+        if to_sell > 0 and hard_liquidate:
+            quantity = to_sell // 2
+            self.sell(true_value, quantity)
+            to_sell -= quantity
+
+        if to_sell > 0 and soft_liquidate:
+            quantity = to_sell // 2
+            self.sell(true_value + 2, quantity)
+            to_sell -= quantity
+
+        if to_sell > 0 and sell_orders:
+            popular_sell_price = min(sell_orders, key=lambda tup: tup[1])[0]
+            price = max(min_sell_price, popular_sell_price - 1)
+            self.sell(price, to_sell)
+
+class RainforestResinStrategy(MarketMakingStrategy):
+    def get_true_value(self, state: TradingState) -> int:
+        return 10_000
+
+class KelpStrategy(MarketMakingStrategy):
+    def get_true_value(self, state: TradingState) -> int:
+        order_depth = state.order_depths[self.symbol]
+
+        order_depth = state.order_depths[self.symbol]
+        buy_orders = sorted(order_depth.buy_orders.items(), reverse=True)
+        sell_orders = sorted(order_depth.sell_orders.items())
+
+        popular_buy_price = max(buy_orders, key=lambda tup: tup[1])[0]
+        popular_sell_price = min(sell_orders, key=lambda tup: tup[1])[0]
+
+        return round((popular_buy_price + popular_sell_price) / 2)
+    
+
+class SquidInkStrategy(Strategy):
+    def act(self, state: TradingState) -> None:
+        order_depth = state.order_depths[self.symbol]
+        current_position = state.position.get(self.symbol, 0)
+        mid_price = self.get_true_value(state)
+        if mid_price > 2120:
+            if abs(current_position) < self.limit:
+                best_bid = max(order_depth.buy_orders.keys(), default = None)
+                print(current_position)
+                self.sell(best_bid, self.limit)
+        elif current_position < 0 and mid_price < 2000:
+            self.buy(mid_price, abs(current_position))
+        if mid_price < 1830:
+            if abs(current_position) < self.limit:
+                best_ask = min(order_depth.sell_orders.keys(), default = None)
+                self.buy(best_ask, self.limit)
+        elif current_position > 0 and mid_price > 1950:
+            self.sell(mid_price, abs(current_position))
+
+    def get_true_value(self, state: TradingState) -> int:
+        order_depth = state.order_depths[self.symbol]
+        buy_orders = sorted(order_depth.buy_orders.items(), reverse=True)
+        sell_orders = sorted(order_depth.sell_orders.items())
+        popular_buy_price = max(buy_orders, key=lambda tup: tup[1])[0]
+        popular_sell_price = min(sell_orders, key=lambda tup: tup[1])[0]
+        return round((popular_buy_price + popular_sell_price) / 2)
+
+    
+class Trader:
+    def __init__(self) -> None:
+        limits = {
+            "RAINFOREST_RESIN": 50,
+            "KELP": 50,
+            "SQUID_INK": 50
+        }
+
+        self.strategies = {symbol: clazz(symbol, limits[symbol]) for symbol, clazz in {
+            "RAINFOREST_RESIN": RainforestResinStrategy,
+            "KELP": KelpStrategy,
+            "SQUID_INK": SquidInkStrategy
+        }.items()}
+
+    def run(self, state: TradingState) -> tuple[dict[str, list[Order]], int, str]:
+        conversions = 0
+        results = {}
+        for symbol, strategy in self.strategies.items():
+            if symbol in state.order_depths:
+                results[symbol] = strategy.run(state)
+
+        return results, conversions, ""
